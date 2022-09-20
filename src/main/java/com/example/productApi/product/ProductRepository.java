5248f3808772070234511c3c61a8008f1d9b6d0b
raigarak <raigaraklol@gmail.com>
2022-09-20 00:00:10 -0400

    Added query to findProductByBrand

--- src/main/java/com/example/productApi/product/ProductRepository.java
+++ src/main/java/com/example/productApi/product/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.example.productApi.product;
 
+import com.example.productApi.brand.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,9 +9,9 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT s FROM Product s WHERE s.brand = ?1")
-    List<Product> findProductByBrand(String brand);
-
     @Query("SELECT s FROM Product s WHERE s.name = ?1")
     Product findProductByName(String name);
+
+    @Query("SELECT s from Product s WHERE s.brand = ?1")
+    Product findProductByBrand(Brand brand);
 }
